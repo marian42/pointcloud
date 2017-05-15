042b8127b864df43d98eeca20253f485a3999a40
Marian <mail@marian42.de>
2017-05-15 22:39:59 +0200

    Color planes depending on orientation

--- Assets/PlaneBehaviour.cs
+++ Assets/PlaneBehaviour.cs
@@ -24,8 +24,20 @@ public class PlaneBehaviour : MonoBehaviour {
 	}
 
 	public void UpdateTransform() {
-		this.transform.localPosition = -this.Plane.normal * this.Plane.distance; ;
+		this.transform.localPosition = -this.Plane.normal * this.Plane.distance;
 		this.transform.rotation = Quaternion.LookRotation(this.Plane.normal);
+	}
+
+	public void UpdateColor() {
+		float inclination = Vector3.Angle(this.Plane.normal, Vector3.up);
+		float direction = Vector3.Angle(this.Plane.normal, Vector3.forward) * (this.Plane.normal.x < 0 ? -1 : 1) + 180.0f;
+		Color color = Color.HSVToRGB(direction / 360.0f, inclination / 60.0f, 1.0f);
+		
+		foreach (var meshRenderer in this.transform.GetComponentsInChildren<MeshRenderer>()) {
+			var tempMaterial = new Material(meshRenderer.sharedMaterial);
+			tempMaterial.color = color;
+			meshRenderer.sharedMaterial = tempMaterial;
+		}
 	}
 
 	public void Start() {
@@ -45,5 +57,6 @@ public class PlaneBehaviour : MonoBehaviour {
 			quad.transform.rotation = Quaternion.Euler(Vector3.right * 180.0f);
 		}
 		this.UpdateTransform();
-	} 
+		this.UpdateColor();
+	}
 }
