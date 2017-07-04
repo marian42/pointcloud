1d5028ffd9bfd1d8e63086bced84bc332cb856af
Marian <mail@marian42.de>
2017-07-04 12:05:02 +0200

    Read xyz files in batches

--- shapefile/XYZSeparator/XYZLoader.cs
+++ shapefile/XYZSeparator/XYZLoader.cs
@@ -7,6 +7,9 @@ using XYZSeparator;
 using System.Globalization;
 
 public class XYZLoader {
+	private const int batchSize = 1000;
+
+
 	public static Vector3[] LoadFile(string fileName) {
 		return File.ReadAllLines(fileName).Select(line => XYZLoader.parseLine(line)).ToArray();
 	}
@@ -17,10 +20,28 @@ public class XYZLoader {
 										  System.IO.FileAccess.Read,
 										  System.IO.FileShare.ReadWrite);
 		var streamReader = new System.IO.StreamReader(filestream, System.Text.Encoding.UTF8, true, 128);
-		string line;
-		while ((line = streamReader.ReadLine()) != null) {
-			yield return parseLine(line);
+
+		while (true) {
+			var batch = readBatch(streamReader);
+			if (!batch.Any()) {
+				yield break;
+			}
+			foreach (var line in batch) {
+				yield return parseLine(line);
+			}
 		}
+	}
+
+	private static List<string> readBatch(StreamReader reader) {
+		var result = new List<string>();
+		for (int i = 0; i < batchSize; i++) {
+			string line = reader.ReadLine();
+			if (line == null) {
+				break;
+			}
+			result.Add(line);
+		}
+		return result;
 	}
 
 	private static Vector3 parseLine(string line) {
