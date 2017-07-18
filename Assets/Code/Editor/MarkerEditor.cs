c698e505194bce34a4c5e406135c7d9cdc7f29d0
Marian <mail@marian42.de>
2017-07-18 02:32:24 +0200

    Add target button to marker

---
+++ Assets/Code/Editor/MarkerEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.IO;
+using System;
+using UnitySlippyMap.Markers;
+
+
+[CustomEditor(typeof(LocationMarkerBehaviour))]
+public class MarkerEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		base.OnInspectorGUI();
+
+		var marker = this.target as LocationMarkerBehaviour;
+
+		if (GUILayout.Button("Target")) {
+			marker.Map.CenterEPSG900913 = marker.CoordinatesEPSG900913;
+		}
+	}
+}
